59a82294f94a7cb7a66d98feec8b8c9851a0f003
Chuck Daniels <chuck@developmentseed.org>
2025-07-03 15:50:49 -0400

    Use multithreading in "diff" response handler to improve throughput (#17)
    * Ignore utcnow deprecation warnings from botocore
    * Use thread pool to improve throughput
    * Use commit-sha in int. test stack to avoid stack
      conflicts when multiple PRs are in flight

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
+
 import json
+from collections.abc import Sequence
 from pathlib import Path
+from typing import Iterator
 
 import boto3
 import pytest
@@ -45,3 +48,60 @@ def cdk_outputs() -> dict[str, str]:
         ),
         {},
     )
+
+
+@pytest.fixture(scope="session")
+def hls_bucket(cdk_outputs: dict[str, str]) -> str:
+    return cdk_outputs["HlsForwardBucketName"]
+
+
+@pytest.fixture(scope="session")
+def lpdaac_bucket(cdk_outputs: dict[str, str]) -> str:
+    return cdk_outputs["LpdaacReconciliationReportsBucketName"]
+
+
+@pytest.fixture(scope="session")
+def response_topic_arn(cdk_outputs: dict[str, str]) -> str:
+    return cdk_outputs["LpdaacResponseTopicArn"]
+
+
+@pytest.fixture(scope="session")
+def report_path() -> Path:
+    return Path("tests") / "fixtures" / "HLS_reconcile_2024239_2.0.json"
+
+
+@pytest.fixture
+def trigger_keys(
+    s3: S3Client, hls_bucket: str, report_path: Path
+) -> Iterator[Sequence[str]]:
+    from hls_lpdaac_reconciliation.response import (
+        group_granule_ids,
+        notification_trigger_key,
+    )
+
+    keys = [
+        notification_trigger_key(granule_id)
+        for (_nfiles, granule_ids) in group_granule_ids(
+            json.loads(report_path.read_text())
+        ).values()
+        for granule_id in granule_ids
+    ]
+
+    for key in keys:
+        # Write trigger file (contents don't matter; we just need a file to "touch")
+        s3.put_object(Bucket=hls_bucket, Key=key, Body=b"{}")
+
+    yield keys
+
+    for key in keys:
+        s3.delete_object(Bucket=hls_bucket, Key=key)
+
+
+@pytest.fixture
+def hls_inventory_reports_bucket(cdk_outputs: dict[str, str]) -> str:
+    return cdk_outputs["HlsInventoryReportsBucketName"]
+
+
+@pytest.fixture
+def lpdaac_request_queue_url(cdk_outputs: dict[str, str]) -> str:
+    return cdk_outputs["LpdaacRequestQueueUrl"]
